a5d4484a69916d4d2470cefba23b13e625904099
otmjka <nindeec@gmail.com>
2014-04-15 02:06:11 +0600

    a bit reduce code

--- spapp.js
+++ spapp.js
@@ -21,7 +21,6 @@
     }
     var ph = pageHandlers[pageName];
     if( ph ) { 
-      $page = $("section#" + pageName);
       var that = $page.length > 0 ? $page[0] : null;
       var r = ph.call(that , param);
       if( typeof r == "function" ) { // it returns the function that's used for view rendering
